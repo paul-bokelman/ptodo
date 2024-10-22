fd3905f8493af79d2c480c2043f175bb46361ffe
Paul Bokelman <paul.bokelman1@gmail.com>
2024-10-22 17:16:09 -0600

    implemented responsive layout

--- apps/web/src/App.tsx
+++ apps/web/src/App.tsx
@@ -5,7 +5,7 @@ import { useSearchParams, useNavigate } from "react-router-dom";
 import { useMutation, useQuery } from "react-query";
 import cn from "clsx";
 import dayjs from "dayjs";
-import { Plus, ArrowRight, ChevronLeft, ChevronRight, ArrowBigDownDash } from "lucide-react";
+import { Plus, ChevronLeft, ChevronRight, ArrowBigDownDash } from "lucide-react";
 import { Task } from "@/components";
 import { Button, Tabs, TabsList, TabsTrigger, useToast } from "@/components/ui";
 import { CreateTaskDialog } from "@/components/dialog";
@@ -63,9 +63,23 @@ const App: React.FC<Props> = () => {
 
   return (
     <>
-      <div className="relative w-screen h-screen flex flex-col gap-2 p-20">
-        <div className="relative flex flex-col">
-          <h1 className="scroll-m-20 text-sm text-muted-foreground tracking-tight">PAB - {date}</h1>
+      <div className="relative w-screen flex flex-col gap-2 p-4">
+        <div className="relative flex flex-row items-center gap-4">
+          <h1 className="scroll-m-20 text-sm text-muted-foreground tracking-tight">PAB</h1>
+          <span className="text-xs text-muted-foreground">—</span>
+          {day?.stats && (
+            <div className="flex items-center gap-3">
+              {[
+                `⚡️ ${day.stats.streak}`,
+                `🏆 ${day.stats.totalCompleted}`,
+                `${day.stats.ratio.incline ? "📈" : "📉"} ${day.stats.ratio.value}`,
+              ].map((value) => (
+                <span key={value} className="text-xs leading-7 text-muted-foreground">
+                  {value}
+                </span>
+              ))}
+            </div>
+          )}
         </div>
 
         <div className="relative flex flex-col gap-4 mt-2">
@@ -99,23 +113,8 @@ const App: React.FC<Props> = () => {
           </div>
           {status === "success" ? (
             <>
-              <div className="flex items-center gap-2">
-                {day.tasks.map((task, i) => (
-                  <span
-                    key={i}
-                    className={cn(
-                      { "text-muted": task.complete, "text-muted-foreground": !task.complete },
-                      "flex items-center gap-1 text-xs"
-                    )}
-                  >
-                    {task.description} {day.tasks.length > i + 1 && <ArrowRight size={12} />}
-                  </span>
-                ))}
-              </div>
-              {!day.tasks.length && (
-                <span className="text-xs text-muted-foreground">No tasks currently, create some!</span>
-              )}
-              <div className="grid grid-cols-3 gap-1">
+              {!day.tasks.length && <span className="text-xs text-muted-foreground">No tasks found, create some!</span>}
+              <div className="grid md:grid-cols-3 gap-1">
                 {day.tasks.map((task, i) => (
                   <Task key={i} {...task} mode={taskMode} />
                 ))}
@@ -125,24 +124,6 @@ const App: React.FC<Props> = () => {
             <span className={cn({ "text-red-500": status === "error" }, "text-xs text-muted-foreground")}>
               {status === "error" ? "Something went wrong" : "Loading tasks..."}
             </span>
-          )}
-        </div>
-        <div className="absolute bottom-20 left-20 flex items-center gap-6">
-          {day?.stats ? (
-            <div className="flex items-center gap-4">
-              {/* this is kinda messy, should it be abstracted or just expanded? */}
-              {[
-                `⚡️ ${day.stats.streak}`,
-                `🏆 ${day.stats.totalCompleted}`,
-                `${day.stats.ratio.incline ? "📈" : "📉"} ${day.stats.ratio.value}`,
-              ].map((value) => (
-                <span key={value} className="text-xs leading-7 text-muted-foreground">
-                  {value}
-                </span>
-              ))}
-            </div>
-          ) : (
-            <span className="text-xs text-muted-foreground">...</span>
           )}
         </div>
       </div>
